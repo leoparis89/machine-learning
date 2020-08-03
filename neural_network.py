266b7465782ede80308786f54ec55fc9e5189c48
Lev Kowalski <levkowalski@gmail.com>
2020-08-03 04:01:56 +0200

    wip

---
+++ neural_network.py
@@ -0,0 +1,21 @@
+
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from keras.datasets import mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+model = models.Sequential([
+    layers.Dense(512, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='rmsprop',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype('float32') / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype('float32') / 255
+
+model.fit(train_images, train_labels, epochs=5, batch_size=128)
