8a453416230bcbb3912caf219857655738e2a937
Lev Kowalski <levkowalski@gmail.com>
2020-08-05 14:38:51 +0200

    wip

--- naive_neural_network.py
+++ naive_neural_network.py
@@ -1,4 +1,6 @@
+from tensorflow.keras.datasets import mnist
 import tensorflow as tf
+import numpy as np
 
 
 class NaiveDense:
@@ -61,3 +63,53 @@ class BatchGenerator:
         labels = self.labels[self.index: self.index + self.batch_size]
         self.index += self.batch_size
         return images, labels
+
+
+def one_training_step(model, images_batch, labels_batch):
+    with tf.GradientTape() as tape:
+        predictions = model(images_batch)
+        per_sample_losses = tf.keras.losses.sparse_categorical_crossentropy(
+            labels_batch, predictions)
+        average_loss = tf.reduce_mean(per_sample_losses)
+    gradients = tape.gradient(average_loss, model.weights)
+    update_weights(gradients, model.weights)
+    return average_loss
+
+
+learning_rate = 1e-3
+
+
+def update_weights(gradients, weights):
+    for g, w in zip(gradients, model.weights):
+        w.assign_sub(w * learning_rate)
+
+
+# training loop
+def fit(model, images, labels, epochs, batch_size=128):
+    for epoch_counter in range(epochs):
+        print('Epoch %d' % epoch_counter)
+        batch_generator = BatchGenerator(images, labels)
+        for batch_counter in range(len(images) // batch_size):
+            images_batch, labels_batch = batch_generator.next()
+            loss = one_training_step(model, images_batch, labels_batch)
+            if batch_counter % 100 == 0:
+                print('loss at batch %d: %.2f' % (batch_counter, loss))
+
+
+# test drive the training loop
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype('float32') / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype('float32') / 255
+
+fit(model, train_images, train_labels, epochs=10, batch_size=128)
+
+
+# evaluate the model
+predictions = model(test_images)
+predictions = predictions.numpy()
+predicted_labels = np.argmax(predictions, axis=1)
+matches = predicted_labels == test_labels
+print('accuracy: %.2f' % matches.average())
