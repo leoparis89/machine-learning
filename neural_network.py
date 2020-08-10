7c61208bbaf70ab439c82db44a1cb9c9efd781ef
Lev Kowalski <levkowalski@gmail.com>
2020-08-10 11:22:58 +0200

    epoch correnction

--- neural_network.py
+++ neural_network.py
@@ -18,7 +18,7 @@ train_images = train_images.astype('float32') / 255
 test_images = test_images.reshape((10000, 28 * 28))
 test_images = test_images.astype('float32') / 255
 
-model.fit(train_images, train_labels, epochs=1, batch_size=128)
+model.fit(train_images, train_labels, epochs=5, batch_size=128)
 
 test_digits = test_images[0:10]
 predictions = model.predict(test_digits)
